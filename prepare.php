053aec3e5c26b1c63cabe615661a2d6d25786e54
lindseyemaddox <lindseyemaddox@gmail.com>
2015-01-08 00:21:34 -0600

    prepare page

---
+++ prepare.php
@@ -0,0 +1,213 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/common.php'); //php framework and meta?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/head.php'); // HTTP head?>
+<link rel="stylesheet" type="text/css" media="print" href="/_assets/css/print.css" />
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/navigation.php'); //navigation?>
+
+<header>
+
+  <img class="temp-image" src="/_assets/img/header.jpg" alt="temporary image">
+
+</header>
+
+<section class="pattern headerComp" id="intro">
+
+	<div class="inner">
+
+		<h2>What to pack</h2>
+
+		<p>The weather during the summer in the Bristol Bay area of southwest Alaska can vary from a warm sunny 75 degrees to a cool rainy 45 degree day. This area is not far from the Bering Sea and Cook Inlet and as the old saying goes, <span class="italic">“if you don’t like the weather just wait a few minutes.”</span> Layer your clothes so that you can add or subtract layers as the weather changes. You will want to bring a light backpack with you daily with your rain gear and possibly a wool sweater to ensure your comfort while in the field. Drag the slider to the left to reveal the illustrated packing list.</p>
+
+	</div><!--inner-->
+
+</section><!--pattern-->
+
+<section id="bgFixed" class="subpage plane">
+  
+  <div class="inner">
+    
+    <div class="left fisherman">
+
+      <img src="/_assets/img/temp-fisherman.png" alt="temp image">
+
+    </div><!--left-->
+
+    <div class="right">
+
+      <table class="dark">
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/warm-cap">warm cap</a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/polarized-sunglasses">polarized sunglasses</a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/long-johns">2 sets of light long johns </a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/wool-socks">2 pairs of padded wool socks</a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/cotton-socks">2 pairs of padded cotton socks</a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/gloves">fishing gloves (fingerless are acceptable)</a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/shirts">2 long-sleeved shirts</a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/wool-sweater">medium-weight wool sweater</a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/pants">2 pairs of wool pants or jeans</a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/down-jacket">light down jacket</a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/rain-suit">rain suit (we recommend Gore-Tex)</a></td>
+        </tr>
+        <tr>
+          <td><a class="fancybox fancybox.iframe" href="/shoes">tennis or deck shoes (for use at lodge)</a></td>
+        </tr>
+      </table>
+
+      <button class="print" onclick="printTable()"><span class="icon-print"></span> Print</button>
+
+    </div><!--right-->
+
+
+  </div><!--inner-->
+
+</section>
+
+<section class="pattern" id="species">
+
+  <div class="inner">
+
+    <h2>Fishing Equipment</h2>
+
+    <p>Rainbow Bay Resort has a complete inventory of fishing equipment, fly and spinning, that we furnish to our clients at no additional cost. Should you wish to bring your own fishing equipment we welcome it. On lures & flies remember only single hooks are allowed in Alaska, double or treble hooks on lures are illegal on trout streams! We recommend that you bring chest waders, Gore-Tex works best, if you have neoprene you need not buy new ones! Remember, felt sole wader boots are illegal in Alaska!</p>
+
+      <table class="pad">
+        <tr>
+          <td class="table-left"><p class="table-header">Spinning Equipment</p><p class="table-body">The equipment listed below is for everything but King Salmon. Kings will weigh upwards of 50 lbs and you need pretty heavy bait casting equipment to land them. We furnish this heavy equipment at the lodge, but if you wish to bring your own equipment for the Kings contact us for proper equipment.</p></td>
+          <td class="no-padding">
+            <table>
+              <tr>
+                <td>Light Weight Spinning Rod</td>
+                <td>1/8 – 1/4 ounce Lures</td>
+              </tr>
+              <tr>
+                <td>Light Weight Reel</td>
+                <td>Loaded with 8 lb Line</td>
+              </tr>
+              <tr>
+                <td>Medium - Heavy Spinning Rod</td>
+                <td>3/8 – 3/4 ounce</td>
+              </tr>
+              <tr>
+                <td>Medium Weight Reel</td>
+                <td>Loaded with 17 lb Test Line</td>
+              </tr>
+              <tr>
+                <td>Chest Waders</td>
+                <td>Gore-Tex Recommended</td>
+              </tr>
+              <tr>
+                <td>Mepps Spinners</td>
+                <td>#3 &amp; #5</td>
+              </tr>
+              <tr>
+                <td>Vibrex Spinners</td>
+                <td>#3 &amp; #5</td>
+              </tr>
+              <tr>
+                <td>Pixie Spoons</td>
+                <td>3/8 – 5/8 ounce</td>
+              </tr>
+              <tr>
+                <td>Dare Devil Spoons</td>
+                <td>3/8 – 5/8 ounce</td>
+              </tr>
+              <tr>
+                <td>Black Crappie Jigs</td>
+                <td>1/8 – 1/4 ounce</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+
+      <button class="print" onclick="printTable()"><span class="icon-print"></span> Print</button>
+
+      <table class="pad pad-more">
+        <tr>
+          <td class="table-left"><p class="table-header">Fly Fishing Equipment</p><p class="table-body">Fly fishing equipment is kind of personal and although we have fly fishing gear at the lodge we suggest you bring your own if you have it. Bringing a fly vest is a good idea if you use our equipment or not, as we do not furnish them. We do furnish flies at the lodge, but bring some of your own to try if you wish.</p></td>
+          <td class="no-padding">
+            <table>
+              <tr>
+                <td>Fly Rod for Trout &amp; Char</td>
+                <td>5 - 6 Weight</td>
+              </tr>
+              <tr>
+                <td>Single &amp; Double Egg Patterns</td>
+                <td>Size 4 &amp; 6</td>
+              </tr>
+              <tr>
+                <td>Fly Rod for Salmon &amp; Pike</td>
+                <td>8 – 9 Weight</td>
+              </tr>
+              <tr>
+                <td>Fly Lines</td>
+                <td>Floating &amp; Sink Tip</td>
+              </tr>
+              <tr>
+                <td>Leaders for Trout &amp; Char</td>
+                <td>8 - 10 pound</td>
+              </tr>
+              <tr>
+                <td>Leaders for Salmon</td>
+                <td>17 - 20 pound</td>
+              </tr>
+              <tr>
+                <td>Steel Leaders for Pike</td>
+                <td>30 – 50 pound</td>
+              </tr>
+              <tr>
+                <td>Bunny or Egg Sucking Leeches</td>
+                <td>Size 2 &amp; 4, Pink &amp; Orange</td>
+              </tr>
+              <tr>
+                <td>Muddler Minnow</td>
+                <td>Size 2, 4 &amp; 6, Black &amp; Purple</td>
+              </tr>
+              <tr>
+                <td>Royal Wolfe Dry Flies</td>
+                <td>Size 12, 14 &amp; 16</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+
+      <button class="print" onclick="printTable()"><span class="icon-print"></span> Print</button>
+
+  </div><!--inner-->
+  
+</section><!--testimonials-->
+
+<script>
+function printTable() {
+    window.print();
+}
+</script>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.fancybox').fancybox();
+  });
+</script>
+<script src="/_assets/js/jquery.fancybox.js"></script>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?>
