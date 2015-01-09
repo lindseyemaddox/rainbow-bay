0fc4a77dd680142b15f69653d330816b9b1fccf3
lindseyemaddox <lindseyemaddox@gmail.com>
2015-01-09 08:51:12 -0600

    more subpages

--- _assets/inc/head.php
+++ _assets/inc/head.php
@@ -7,7 +7,7 @@
 
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=1027">
 	<title>Alaska's Rainbow Bay Resort</title>
 	<meta name="description" content="Alaska's premiere fishing resort, Rainbow Bay.">
 	
---
+++ lodges.php
@@ -0,0 +1,148 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/common.php'); //php framework and meta?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/head.php'); // HTTP head?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/navigation.php'); //navigation?>
+
+<header>
+
+  <img class="temp-image" src="/_assets/img/header.jpg" alt="temporary image">
+
+</header>
+
+<section class="pattern headerComp" id="intro">
+
+	<div class="inner">
+
+		<h2>The Lodges</h2>
+
+		<p>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</p>
+
+	</div><!--inner-->
+
+</section><!--pattern-->
+
+<section id="bgFixed" class="subpage lodge">
+  
+  <div class="inner">
+
+    <img class="lodge-logo" src="/_assets/img/logo-eagles-peak.png" alt="Eagle's Peak lodge logo">
+
+    <p>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska. Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar. You will spend your evenings in the spacious lounging area and while you re-live your days fishing adventures, you will enjoy your favorite beverage and hors d'oeuvres before setting down to a sumptuously prepared meal. <a href="/rates" class="fancybox fancybox.iframe readmore">View rates</a></p>
+
+    <div class="slider">
+    
+      <div class="flexslider carousel">
+  
+        <ul class="slides">
+
+          <li>
+            <img src="/_assets/img/lodges-overhead.jpg" alt="overhead view of the Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+          <li>
+            <img src="/_assets/img/lodges-bedroom.jpg" alt="bedroom in Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+          <li>
+            <img src="/_assets/img/lodges-dining.jpg" alt="dining room at the Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+          <li>
+            <img src="/_assets/img/lodges-overhead.jpg" alt="Overhead view of the Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+          <li>
+            <img src="/_assets/img/lodges-overhead.jpg" alt="Overhead view of the Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+          <li>
+            <img src="/_assets/img/lodges-overhead.jpg" alt="Overhead view of the Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+        </ul><!--slides-->
+
+      </div><!--flexslider-->
+
+    </div><!--slider-->
+
+    <img class="lodge-logo second-logo" src="/_assets/img/logo-angry-eagle.png" alt="Angry Eagle lodge logo">
+
+    <p>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska. Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar. <a href="/rates" class="fancybox fancybox.iframe readmore">View rates</a></p>
+
+    <div class="slider">
+    
+      <div class="flexslider carousel">
+  
+        <ul class="slides">
+
+          <li>
+            <img src="/_assets/img/lodges-overhead.jpg" alt="overhead view of the Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar. </h3>
+          </li>
+
+          <li>
+            <img src="/_assets/img/lodges-bedroom.jpg" alt="bedroom in Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+          <li>
+            <img src="/_assets/img/lodges-dining.jpg" alt="dining room at the Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+          <li>
+            <img src="/_assets/img/lodges-overhead.jpg" alt="Overhead view of the Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+          <li>
+            <img src="/_assets/img/lodges-overhead.jpg" alt="Overhead view of the Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+          <li>
+            <img src="/_assets/img/lodges-overhead.jpg" alt="Overhead view of the Eagle's Peak Lodge">
+            <h3>Rainbow Bay Resort Lodge is one of the most luxurious, yet rustic, lodges in Alaska.   Situated on the remote shores of Lake Iliamna, we offer a quiet serenity and fantastic fishing seldom found anywhere.  Each bedroom is spaciously furnished with two queen size beds and a private bath for your comfort and convenience.  The Lodge is constructed with logs and the interior is covered with cedar.</h3>
+          </li>
+
+        </ul><!--slides-->
+
+      </div><!--flexslider-->
+
+    </div><!--slider-->
+
+  </div><!--inner-->
+  
+</section><!--testimonials-->
+
+<script type="text/javascript">
+  $(window).load(function(){
+      $('.flexslider').flexslider({
+        animation: "slide",
+        directionNav: true,
+        pauseOnHover: true,
+        minItems: 3,
+        maxItems: 5,
+        itemWidth: 340,
+        slideshow: false, 
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+  });
+</script>
+<script src="/_assets/js/jquery.flexslider.js"></script>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.fancybox').fancybox();
+  });
+</script>
+<script src="/_assets/js/jquery.fancybox.js"></script>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/_assets/inc/footer.php'); // footer, close body and html?>
