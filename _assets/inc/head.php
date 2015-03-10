661e5e4d173af2e1f3847bcf862bcd2f8b253042
Lindsey Maddox <lindseyemaddox@gmail.com>
2015-03-10 17:01:33 -0500

    twitter card

--- _assets/inc/head.php
+++ _assets/inc/head.php
@@ -10,6 +10,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Angry Eagle Lodge & Outfitters</title>
 	<meta name="description" content="Alaska's premier fishing resort, Angry Eagle Lodge & Outfitters">
+
+	<!--twitter card-->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@angryeaglelodge">
+	<meta name="twitter:creator" content="@anngryeaglelodge">
+	<meta name="twitter:title" content="Angry Eagle Lodge & Outfitters">
+	<meta name="twitter:description" content="Angry Eagle Lodge & Outfitters operates two full-service fishing lodges in the heart of Southwest Alaska’s Bristol Bay region. For the past 30 years, we’ve prided ourselves on offering the fishing adventures of a lifetime.">
+	<meta name="twitter:image:src" content="http://www.angryeagle.com/emails/twitter-card.jpg">
 	
 	<!--[if lt IE 9]>
 	    <p class="browsehappy">You are using an outdated browser. Please <a href="http://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.</p>
@@ -22,6 +30,7 @@
 
 	<!-- Stylesheets -->
 	<link rel="stylesheet" href="/_assets/css/styles.css" media="screen">
+	<link rel="stylesheet" href="/smTemplate/css/ae-admin.css" type="text/css">
 
 	<!-- Fonts -->
 	<link href='http://fonts.googleapis.com/css?family=Passion+One:700|Condiment|Source+Sans+Pro:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
@@ -30,14 +39,18 @@
 	<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="/_assets/js/main.js"></script>
-	
+
 	<!--google recaptcha-->
 	<script src='https://www.google.com/recaptcha/api.js'></script>
+
+	<!--google analytics-->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 	
-	<!--twitter card-->
-	<meta name="twitter:card" content="summary_large_image">
-	<meta name="twitter:site" content="@angryeaglelodge">
-	<meta name="twitter:creator" content="@lindseyemaddox">
-	<meta name="twitter:title" content="Angry Eagle Lodge & Outfitters">
-	<meta name="twitter:description" content="Angry Eagle Lodge & Outfitters operates two full-service fishing lodges in the heart of Southwest Alaska’s Bristol Bay region. For the past 30 years, we’ve prided ourselves on offering the fishing adventures of a lifetime.">
-	<meta name="twitter:image:src" content="http://www.angryeagle.com/emails/twitter-card.jpg">
+	  ga('create', 'UA-60606714-1', 'auto');
+	  ga('send', 'pageview');
+	
+	</script>
