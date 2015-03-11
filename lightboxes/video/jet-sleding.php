cce4bdd12855dc6cc8716ece9712082d864463b9
lindseyemaddox <lindseyemaddox@gmail.com>
2015-03-11 12:50:07 -0500

    2 more

---
+++ lightboxes/video/jet-sleding.php
@@ -0,0 +1,10 @@
+<?php require_once('../_assets/inc/head.php'); // HTTP head?>
+
+	<section>
+
+		<iframe property="video" width="100%" height="500" src="//www.youtube.com/embed/sxe-m2lvCqw" frameborder="0" allowfullscreen></iframe>
+
+	</section>
+
+  </body>
+</html>
