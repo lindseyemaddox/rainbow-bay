44f9172644f47024155c598e0426eaa62980b4fe
Lindsey Maddox <lindseyemaddox@gmail.com>
2015-03-25 09:28:54 -0500

    404 page

---
+++ 404.php
@@ -0,0 +1,36 @@
+<?php require_once('../_assets/inc/common.php'); //php framework and meta?>
+<?php require_once('../_assets/inc/head.php'); // HTTP head?>
+<title>Angry Eagle Lodge & Outfitters | </title>
+<meta name="description" content="Go Fish! We're sorry the page you're after is elusive.">
+<?php require_once('../_assets/inc/subpage-header.php'); //video header?>
+<?php require_once('../_assets/inc/navigation.php'); //navigation?>
+
+<section class="pattern dark" id="fourohfour">
+
+	<div class="inner">
+
+		<h2>Go Fish!</h2>
+
+		<h3 class="alt" style="line-height:1.25em;">Try again! The page you're looking for is not available. Please try the links below to get back after that catch!</h3>
+		
+		<p class="first"><a href="/">Home</a></p>
+		
+		<p><a href="/about/owners">About Our Owners</a></p>
+
+		<p><a href="/about/lodges">About Our Lodges</a></p>
+
+		<p><a href="/galleries/">Pictures of Fish!</a></p>
+
+		<p><a href="/contact">I still haven't found what I'm looking for.</a></p>
+
+	</div><!--inner-->
+
+</section><!--pattern-->
+
+<script type="text/javascript">
+    jQuery(function($){
+      $("input[name='phone']").mask("(999) 999-9999");
+    });
+</script>
+
+<?php require_once('../_assets/inc/footer.php'); // footer, close body and html?>
