bd69f819990d1839d8e9f5cda65de41b4301009a
lindseyemaddox <lindseyemaddox@gmail.com>
2015-10-17 13:42:06 -0500

    sitemap edits, corporate retreats page (not in nav yet)

---
+++ recreation/retreats.php
@@ -0,0 +1,53 @@
+<?php require_once('../_assets/inc/common.php'); //php framework and meta?>
+<?php require_once('../_assets/inc/head.php'); // HTTP head?>
+<title>Alaskan Corporate Retreats | Angry Eagle</title>
+<meta name="description" content="The Alaskan wilderness is one of the lasting treasures in North America. Join us to explore the beauty of Alaska." />
+<link rel="canonical" href="http://www.angryeagle.com/recreation/retreats" />
+<?php require_once('../_assets/inc/subpage-header.php'); //video header?>
+<?php require_once('../_assets/inc/navigation.php'); //navigation?>
+
+<section class="pattern headerComp" id="intro">
+
+	<div class="inner">
+
+		<h1>Alaskan Corporate Getaways & Staff Retreats</h1>
+
+		<p>When’s the last time you looked up from a business meeting to see the splendor of Southwestern Alaska right outside your window?</p>
+
+		<p>Escape from daily distractions of the office so you can focus on your company’s future by combining your next corporate getaway or group team building retreat with the experience of a lifetime at picturesque Angry Eagle Lodge & Outfitters in the heart of Southwest Alaska’s Bristol Bay region.</p>
+
+		<p>Angry Eagle provides a restful retreat where your employees can find inspiration and tap into their creativity. You’ll have everything you need during your corporate retreat, including:</p>
+		
+		<ul>
+			<li>Upscale lodging and accommodations for small company events of 24 guests or fewer</li>
+			<li>Delectable cuisine—breakfast, lunch, and dinner—prepared fresh by our in-house chef</li>
+			<li>Private space for group meetings</li>
+			<li>Access to satellite television and high-speed wireless Internet</li>
+			<li>Recreational activities, such as fly fishing, plane tours, boating, and hiking</li>
+		</ul>
+
+		<h2 class="subh2">Incentive Travel Programs & Employee Incentive Plans</h2>
+
+		<p>Looking for a way to reward staff members for their hard work and retain your most productive employees? Whether you want to motivate your sales team or reward your top performers, Angry Eagle offers a unique opportunity to recognize a job well done.</p>
+		
+		<p>Other resorts may offer the promise of seclusion, but Angry Eagle guarantees it—especially considering we’re so remote that we don’t even have an address. Contact us for more information on our corporate meeting planning and corporate group travel rates.</p>
+
+	</div><!--inner-->
+
+</section><!--pattern-->
+
+<section class="pattern" id="links">
+
+  <div class="inner">
+
+    <p class="prev"><a href="/recreation/sportfishing/"><img src="/_assets/img/prev.png"/> <span>Previous:</span> Sportfishing</a></p>
+
+    <p class="next"><a href="/galleries/"><span>Next:</span> Galleries <img src="/_assets/img/next.png"/></a></p>
+
+    <div class="clearfix"></div>
+
+  </div><!--inner-->
+
+</section>
+
+<?php require_once('../_assets/inc/footer.php'); // footer, close body and html?>
